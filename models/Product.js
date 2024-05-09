76165d3f6ddd0473a68e7abd81b1f8e4647a7029
dmharrison <dmharrison2014@gmail.com>
2024-05-08 22:46:19 -0400

    fixed key error in product to upload the seed data

--- models/Product.js
+++ models/Product.js
@@ -39,7 +39,7 @@ Product.init(
       type: DataTypes.INTEGER,
       references: {
         model: "category",
-        key: "key",
+        key: "id",
       },
     },
   },
--- models/index.js
+++ models/index.js
@@ -19,7 +19,7 @@ Category.hasMany(Product, {
 Product.belongsToMany(Tag, { through: ProductTag });
 
 // Tags belongToMany Products (through ProductTag)
-Tag.belongToMany(Product, { through: ProductTag });
+Tag.belongsToMany(Product, { through: ProductTag });
 
 module.exports = {
   Product,
